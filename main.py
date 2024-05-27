5664c2d8015294679fd294fa15b33c65ace271dc
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2024-05-27 19:12:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -243,7 +243,7 @@ with st.sidebar:
         with st.chat_message("assistant"):
 
             st.markdown(gemini_response.text)
-            elif selected == "VisionBot":
+    elif selected == "VisionBot":
     st.header("👁 VisionBot - Money Maven")
     st.write("")
 
